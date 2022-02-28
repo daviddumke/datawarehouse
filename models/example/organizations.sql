2a4e7ca564053fb21213d79d609b15b7d638eefb
David Dumke <david.dumke@gmail.com>
2022-02-28 19:55:40 +0000

    Table organizações criada.

---
+++ models/example/organizations.sql
@@ -0,0 +1,25 @@
+
+/*
+    Welcome to your first dbt model!
+    Did you know that you can also configure models directly within SQL files?
+    This will override configurations stated in dbt_project.yml
+
+    Try changing "table" to "view" below
+*/
+
+{{ config(materialized='table') }}
+
+with stg_organizations as (
+
+    SELECT * FROM pipedrive.organizations LIMIT 1000
+
+)
+
+select *
+from stg_organizations
+
+/*
+    Uncomment the line below to remove records with null `id` values
+*/
+
+-- where id is not null
